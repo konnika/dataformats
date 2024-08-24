ea6f082f9c09e0b6d736bdef57c0355c9d4be0fe
Andreas Konrad <Andreas.Konrad@msg.group>
2024-08-24 22:30:50 +0200

    Add files from spring initializer

---
+++ src/main/java/de/konni/msg/dataformats/DataformatsApplication.java
@@ -0,0 +1,13 @@
+package de.konni.msg.dataformats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataformatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataformatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/konni/msg/dataformats/DataformatsApplicationTests.java
@@ -0,0 +1,13 @@
+package de.konni.msg.dataformats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataformatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
